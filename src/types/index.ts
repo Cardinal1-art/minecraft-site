540ee1e08da54ec95a7f22aabf5a594e7c0f9383
Cardinal1-art <m1katsu@protonmail.com>
2025-06-27 22:59:17 +0300

    Added index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+export interface Language {
+  code: string;
+  name: string;
+  flag: string;
+}
+
+export interface Translations {
+  [key: string]: {
+    [key: string]: string;
+  };
+}
